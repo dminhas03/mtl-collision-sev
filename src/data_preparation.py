e7c0e31a6b50a454a08ed053cc43dc2f81e8f2b6
Daljit Minhas <dnminhas88@icloud.com>
2025-08-12 23:58:54 -0400

    Ignore parquet data files and add data preparation

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 
 import sys
 if __name__ == "__main__" and (Path.cwd() / "src").exists():
-    # ensure project root is on sys.path when run as a script
+    # make sure project root is on sys.path when run as a script
     sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from src.utils_io import load_filtered, save_processed
+from src.utils_io import load_raw, save_processed
 
 def handle_time(df):
     # Extract year, month, and day from date of accident
@@ -28,6 +29,11 @@ def handle_time(df):
     "DI": 6   # Dimanche - Sunday
     }
     
+    # Formatting hour variable (makeing it datetime)
+    start_times = df["HEURE_ACCDN"].astype("string").str.split("-").str[0]
+    df["hour"] = pd.to_datetime(start_times, format="%H:%M:%S", errors="coerce").dt.hour
+
+    # Creating weekend flag to identify if a day is a weekend
     df["weekday"] = df["JR_SEMN_ACCDN"].map(day_map)
     df["is_weekend"] = (df["weekday"] >= 5).astype(int)
 
@@ -62,7 +68,7 @@ def missing_data(df):
         if pd.notna(med):
             df["hour"] = df["hour"].fillna(med).astype(int)
         else:
-            df["hour"] = df["hour"].fillna(0).astype(int)  # fallback
+            df["hour"] = df["hour"].fillna(0).astype(int)  # If median doesn't exist, set 0
 
     # Categorical columns: fill with "N/A"
     cat_candidates = [
@@ -75,3 +81,52 @@ def missing_data(df):
         df[c] = df[c].astype("string").fillna("N/A")
 
     return df
+
+def encode(df):
+    cat_cols = df.select_dtypes(include=["object", "string", "category"]).columns.tolist()
+    cat_cols = [c for c in cat_cols if c not in ["GRAVITE", "JR_SEMN_ACCDN", "HEURE_ACCDN"]]
+
+    enc = OneHotEncoder(drop="first", sparse_output=False, handle_unknown="ignore")
+    enc_arr = enc.fit_transform(df[cat_cols])
+    enc_df = pd.DataFrame(enc_arr, columns=enc.get_feature_names_out(cat_cols), index=df.index)
+
+    df = pd.concat([df.drop(columns=cat_cols), enc_df], axis=1)
+    return df
+
+
+def main():
+    # Load raw data into dataframe
+    df = load_raw()
+    
+    # Columns we want to keep:
+    keep_cols = [
+    # Target variable
+    'GRAVITE',
+    # Casualty counts
+    'NB_MORTS', 'NB_BLESSES_GRAVES', 'NB_BLESSES_LEGERS',
+    'NB_VICTIMES_TOTAL',
+    'NB_DECES_PIETON', 'NB_BLESSES_PIETON', 'NB_VICTIMES_PIETON',
+    'NB_DECES_MOTO', 'NB_BLESSES_MOTO', 'NB_VICTIMES_MOTO',
+    'NB_DECES_VELO', 'NB_BLESSES_VELO', 'NB_VICTIMES_VELO',
+
+    # Time features
+    'DT_ACCDN', 'JR_SEMN_ACCDN', "HEURE_ACCDN",
+
+    # Location features
+    'LOC_LAT', 'LOC_LONG', 'CD_CATEG_ROUTE', 'CD_SIT_PRTCE_ACCDN',
+
+    # Environment and context
+    'CD_COND_METEO', 'CD_ETAT_SURFC', 'CD_ECLRM', 'CD_ENVRN_ACCDN', 'CD_GENRE_ACCDN'
+    ]
+    df = df[keep_cols].copy()
+
+    # Apply methods
+    df = handle_time(df)
+    df = missing_data(df)
+    df = encode(df)
+
+    # Save df
+    save_processed(df)
+
+if __name__ == "__main__":
+    main()
